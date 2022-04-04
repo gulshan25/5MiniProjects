7d483280182fa1ab2072e44e291003d25d8d8683
gulshan25 <g.r.tanny@gmail.com>
2022-04-05 05:59:21 +0600

    5 mini projects

--- project1/blurryLoading/script.js
+++ project1/blurryLoading/script.js
@@ -6,15 +6,18 @@ let load = 0;
 let int = setInterval(blurring, 30);
 
 function blurring() {
-    load++;
+    // showing error message
+    load++; // showing error message
 
     if (load > 99) {
-        clearInterval(int);
+        clearInterval(int); // showing error message
     }
 
     loadText.innerHTML = `${load}%`;
     loadText.style.opacity = scale(load, 0, 100, 1, 0);
     bg.style.filter = `blur(${scale(load, 0, 100, 30, 0)}px)`;
+
+    // showing  scale error message
 }
 
 // const scale = (num, in_min, in_max, out_min, out_max) => {
