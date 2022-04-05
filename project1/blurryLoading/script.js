c98853d5c4f1962860c62001dd42eacd09ac4fad
gulshan25 <g.r.tanny@gmail.com>
2022-04-05 06:06:22 +0600

    5 mini projects

--- project1/blurryLoading/script.js
+++ project1/blurryLoading/script.js
@@ -20,6 +20,8 @@ function blurring() {
     // showing  scale error message
 }
 
-// const scale = (num, in_min, in_max, out_min, out_max) => {
-//     return ((num - in_min) * (out_max - out_min)) / (in_max - in_min) + out_min;
-// };
+// https://stackoverflow.com/questions/10756313/javascript-jquery-map-a-range-of-numbers-to-another-range-of-numbers
+
+const scale = (num, in_min, in_max, out_min, out_max) => {
+    return ((num - in_min) * (out_max - out_min)) / (in_max - in_min) + out_min;
+};
